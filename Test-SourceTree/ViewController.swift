a50ead8fb29f8adec3105da010c1e8b9079f674c
Wappik <wappik4@gmail.com>
2022-07-13 18:38:15 +0300

    Initial Commit

---
+++ Test-SourceTree/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test-SourceTree
+//
+//  Created by Эдуард Дума on 13.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
